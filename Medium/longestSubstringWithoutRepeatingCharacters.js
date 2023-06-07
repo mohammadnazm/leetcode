e9038b70fda2f65d55c9be5fb7c72c5f6896a53a
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2023-06-08 00:03:09 +0300

    done

--- Medium/longestSubstringWithoutRepeatingCharacters.js
+++ Medium/longestSubstringWithoutRepeatingCharacters.js
@@ -2,17 +2,19 @@
  * @param {string}
  * @return {number}
  */
-    var lengthOfLongestSubstring = function(s) {
-    let ans = 0, l = 0, r = 0;
-    let n = s.length;
-    let map = new Array(256).fill(-1);
-    while (r < n) {
-        if (map[s.charCodeAt(r)] != -1) {
-            l = Math.max(map[s.charCodeAt(r)] + 1, l);
-        }
-        map[s.charCodeAt(r)] = r;
-        ans = Math.max(ans, r - l + 1);
-        r++;
+var lengthOfLongestSubstring = function (s) {
+  let ans = 0,
+    l = 0,
+    r = 0
+  let n = s.length
+  let map = new Array(256).fill(-1)
+  while (r < n) {
+    if (map[s.charCodeAt(r)] != -1) {
+      l = Math.max(map[s.charCodeAt(r)] + 1, l)
     }
-    return ans;
-};
+    map[s.charCodeAt(r)] = r
+    ans = Math.max(ans, r - l + 1)
+    r++
+  }
+  return ans
+}
